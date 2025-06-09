891b45c93f6c73fbef1672dbbf62ef7fe7dc8d14
Tan Nguyen <bivani2805@gmail.com>
2025-06-09 02:06:06 -1000

    Package the application into one jar file for both front end and back end and README.md instruction

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # WatcherHW_webapp
+WatcherHW is an open source web application that monitor hardware performance at run time.
+The app gets hardware information using python and display it every 5 seconds.
+
+# How the apps work
+It gets hardware information using python. 
+Python scripts are run using Java Runtime library.
+Then information is displayed through Java Spring Boot as a backend.
+The frontend is in React Typescript
+
+# Version
+The current version is only available for Windows.
+This is not a CRUD web application. In order to monitor your computer hardware you need to download and run it.
+
+# Requirements
+Make sure you have python install and java install.
+
+# How to install
+1. Download python_scripts folder
+2. Download watcherhw-0.0.1-SNAPSHOT.jar
+3. Keep the python_scripts folder and .jar file in the same folder
+
+# How to run
+Open your command prompt for windows.
+cd into the directory that have the .jar file and python_scripts
+Run
+java -jar watcherhw-0.0.1-SNAPSHOT.jar
---
+++ python_scripts/run_admin.py
@@ -0,0 +1,27 @@
+import json
+import admin
+import sys
+# print("Are you root? ", end="")
+# Check if root or not
+# https://github.com/raspiduino/pythonadmin
+if not admin.isUserAdmin():
+    if len(sys.argv) == 2:
+        if sys.argv[1] == 'cpu_temps':
+            print(json.dumps("Getting cpu details..."))
+            admin.runAsAdmin(f"python -i ./python_scripts/get-cpudetails.py -i temp") # Run command as root
+        if sys.argv[1] == 'cpu_clocks':
+            print(json.dumps("Getting cpu details..."))
+            admin.runAsAdmin(f"python -i ./python_scripts/get-cpudetails.py -i clock") # Run command as root
+        if sys.argv[1] == 'cpu_powers':
+            print(json.dumps("Getting cpu details..."))
+            admin.runAsAdmin(f"python -i ./python_scripts/get-cpudetails.py -i pow") # Run command as root
+        if sys.argv[1] == 'cpu_vols':
+            print(json.dumps("Getting cpu details..."))
+            admin.runAsAdmin(f"python -i ./python_scripts/get-cpudetails.py -i vol") # Run command as root
+        if sys.argv[1] == 'gpu_power':
+            print(json.dumps("Getting GPU Details..."))
+            admin.runAsAdmin(f"python -i ./python_scripts/get-gpudetails.py")
+    
+else:
+    print(json.dumps("Now you are root!"))
+    input("")
